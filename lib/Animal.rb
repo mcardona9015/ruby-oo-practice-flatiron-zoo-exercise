60fe849a0e84efb97bcdaa975cc0215eeb4173d6
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-11 12:16:54 -0500

    nice work

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,4 +1,21 @@
 class Animal
+attr_accessor :weight
+attr_reader :nickname, :species
+
+@@all = []
+
+def initialize(nickname, species, weight)
+    @nickname = nickname
+    @species = species
+    @weight = weight
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+
 
 end
 #test
