b7f06cf001cc727719735cb021ba615dd49284af
mcardona9015 <mcardona9015@gmail.com>
2020-12-13 10:45:58 -0500

    cit-country method

--- lib/City.rb
+++ lib/City.rb
@@ -3,14 +3,14 @@ require 'pry'
 class City
 
     attr_accessor :zoo, :animal
-    attr_reader :name, :origin
+    attr_reader :name, :country
 
     @@all = []
 
     def initialize(animal, name_param, country_param, zoo)
         @animal = animal
         @name = name_param
-        @origin = country_param    
+        @country = country_param    
         @zoo = zoo
         @@all << self    
     end
@@ -20,7 +20,7 @@ class City
     end
 
     def country
-        self.origin
+        self.country
     end
 
 
